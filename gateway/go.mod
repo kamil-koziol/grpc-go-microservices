b39f6a1a314d690371639cce3f2c23a9348a8b9c
Kamil Kozioł <koziol.kamil@protonmail.com>
2024-11-24 13:33:28 +0100

    Add 'rabbitmq' message broker support

--- gateway/go.mod
+++ gateway/go.mod
@@ -1,6 +1,14 @@
 module github.com/kamil-koziol/gateway
 
-
 go 1.22.5
 
-require github.com/joho/godotenv v1.5.1 // indirect
+require (
+	github.com/joho/godotenv v1.5.1
+	google.golang.org/grpc v1.67.1
+)
+
+require (
+	golang.org/x/sys v0.24.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240814211410-ddb44dafa142 // indirect
+	google.golang.org/protobuf v1.34.2 // indirect
+)
--- orders/grpc_handler.go
+++ orders/grpc_handler.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"log"
 
 	pb "github.com/kamil-koziol/common/api"
+	"github.com/kamil-koziol/common/broker"
+	amqp "github.com/rabbitmq/amqp091-go"
 	"google.golang.org/grpc"
 )
 
@@ -12,19 +15,39 @@ type grpcHandler struct {
 	pb.UnimplementedOrderServiceServer
 
 	service OrdersService
+	channel *amqp.Channel
 }
 
-func NewGRPCHandler(grpcServer *grpc.Server, service OrdersService) {
+func NewGRPCHandler(grpcServer *grpc.Server, service OrdersService, channel *amqp.Channel) {
 	handler := &grpcHandler{
 		service: service,
+		channel: channel,
 	}
 	pb.RegisterOrderServiceServer(grpcServer, handler)
 }
 
 func (h *grpcHandler) CreateOrder(ctx context.Context, payload *pb.CreateOrderRequest) (*pb.Order, error) {
 	log.Printf("New order received! %v", payload)
-	return &pb.Order{
+    o := &pb.Order{
 		ID:         "42",
 		CustomerID: payload.CustomerId,
-	}, nil
+	}
+
+    marshalledOrder, err := json.Marshal(o)
+    if err != nil {
+        return nil, err
+    }
+
+    q, err := h.channel.QueueDeclare(broker.OrderCreatedEvent,true, false, false, false, nil)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    h.channel.PublishWithContext(ctx, "", q.Name, false, false, amqp.Publishing{
+        ContentType: "application/json",
+        Body: marshalledOrder,
+        DeliveryMode: amqp.Persistent,
+    })
+
+    return o, nil
 }
